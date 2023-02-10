309e8ded865f89478f328f9a4249365a04d3134e
shchansky <gavrilenko71285@gmail.com>
2023-02-10 18:54:24 +0300

    commit

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { Icons } from "assets";
 import * as Markup from "./toolbar.styles";
 
 export const Toolbar = () => {
   return (
     <Markup.Container>
-      <Markup.Button></Markup.Button>
+      <Markup.Tools>
+        <Markup.Button>
+          <Icons.BrushIcon />
+        </Markup.Button>
+        <Markup.Button>
+          <Icons.RectIcon />
+        </Markup.Button>
+        <Markup.Button>
+          <Icons.CircleIcon />
+        </Markup.Button>
+        <Markup.Button>
+          <Icons.EraserIcon />
+        </Markup.Button>
+        <Markup.Button>
+          <Icons.LineIcon />
+        </Markup.Button>
+        <Markup.Button>
+          <Icons.PaletteIcon />
+        </Markup.Button>
+      </Markup.Tools>
+      <Markup.Menu>
+        <Markup.Button>
+          <Icons.UndoIcon />
+        </Markup.Button>
+        <Markup.Button>
+          <Icons.RedoIcon />
+        </Markup.Button>
+        <Markup.Button>
+          <Icons.SaveIcon />
+        </Markup.Button>
+      </Markup.Menu>
     </Markup.Container>
   );
 };
