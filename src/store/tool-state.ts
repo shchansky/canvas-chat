e726475d9e8b8c56a9cd60f98e8a3bb91b565fcd
shchansky <gavrilenko71285@gmail.com>
2023-02-10 19:12:01 +0300

    commit

---
+++ src/store/tool-state.ts
@@ -0,0 +1,14 @@
+import { makeAutoObservable } from "mobx";
+
+export class ToolState {
+  public tool: any | null = null;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  public setTools(tool: any| null) {
+
+    this.tool = tool
+  };
+}
