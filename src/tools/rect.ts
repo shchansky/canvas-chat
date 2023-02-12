930f64318502b4a9ae5fdb07649eb77ea61b23b9
shchansky <gavrilenko71285@gmail.com>
2023-02-12 11:51:09 +0300

    commit

--- src/tools/rect.ts
+++ src/tools/rect.ts
@@ -70,9 +70,9 @@ import Tool from "./tool";
 
 export default class Rect extends Tool {
   public isMouseDown: boolean = false;
-  public startX: number = 0
-  public startY: number = 0
- public saved: string =""
+  public startX: number = 0;
+  public startY: number = 0;
+  public saved: string = "";
 
   constructor(canvas: HTMLCanvasElement) {
     super(canvas);
@@ -88,11 +88,9 @@ export default class Rect extends Tool {
 
   mouseUpHandler(e: MouseEvent) {
     this.isMouseDown = false;
-
   }
   mouseDownHandler(e: MouseEvent) {
-
-    if( !this.ctx) return
+    if (!this.ctx) return;
 
     this.isMouseDown = true;
     this.ctx.beginPath();
@@ -105,15 +103,15 @@ export default class Rect extends Tool {
   }
   mouseMoveHandler(e: MouseEvent) {
     if (this.isMouseDown) {
-    // @ts-ignore:next-line
+      // @ts-ignore:next-line
       let currentX = e.pageX - e.target.offsetLeft;
-    // @ts-ignore:next-line
+      // @ts-ignore:next-line
       let currentY = e.pageY - e.target.offsetTop;
-    // @ts-ignore:next-line
+      // @ts-ignore:next-line
       this.width = currentX - this.startX;
-    // @ts-ignore:next-line
+      // @ts-ignore:next-line
       this.height = currentY - this.startY;
-    // @ts-ignore:next-line
+      // @ts-ignore:next-line
       this.draw(this.startX, this.startY, this.width, this.height);
     }
   }
@@ -122,20 +120,18 @@ export default class Rect extends Tool {
     const img = new Image();
     img.src = this.saved;
     img.onload = () => {
-    // @ts-ignore:next-line
+      // @ts-ignore:next-line
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    // @ts-ignore:next-line
+      // @ts-ignore:next-line
       this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
-    // @ts-ignore:next-line
+      // @ts-ignore:next-line
       this.ctx.beginPath();
-    // @ts-ignore:next-line
+      // @ts-ignore:next-line
       this.ctx.rect(x, y, w, h);
-    // @ts-ignore:next-line
+      // @ts-ignore:next-line
       this.ctx.fill();
-    // @ts-ignore:next-line
+      // @ts-ignore:next-line
       this.ctx.stroke();
     };
   }
-
-
 }
