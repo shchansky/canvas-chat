ca1a3b1ba831a09c28e4630826467297bbe6045a
shchansky <gavrilenko71285@gmail.com>
2023-02-12 11:36:10 +0300

    commit

--- src/tools/rect.ts
+++ src/tools/rect.ts
@@ -1,67 +1,141 @@
-import { throws } from "assert";
+// import { throws } from "assert";
+// import Tool from "./tool";
+
+// export default class Rect extends Tool {
+//   public isMouseDown: boolean = false;
+
+//   constructor(canvas: HTMLCanvasElement) {
+//     super(canvas);
+//     this.listen();
+//   }
+
+//   public listen() {
+//     if (!this.canvas) return;
+
+//     this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
+//     this.canvas.onmousedown = this.mouseDownHandler.bind(this);
+//     this.canvas.onmouseup = this.mouseUpHandler.bind(this);
+//   }
+
+//   public mouseUpHandler(ev: MouseEvent) {
+//     this.isMouseDown = false;
+//   }
+
+//   public mouseDownHandler(ev: MouseEvent) {
+//     if (!this.ctx) return;
+//     this.isMouseDown = true;
+//     /** beginPath() - метод вызывается у контекста canvas, который говрит о начале отрисовки новой фигуры  */
+//     this.ctx.beginPath();
+//     // @ts-ignore:next-line
+//     this.startX = ev.pageX - ev.target.offsetLeft;
+//     // @ts-ignore:next-line
+//     this.startY = ev.pageY - ev.target.offsetTop;
+//   }
+
+//   public mouseMoveHandler(ev: MouseEvent) {
+//     if (this.isMouseDown) {
+//       // @ts-ignore:next-line
+//       let currentX = ev.pageX - ev.target.offsetLeft;
+//       // @ts-ignore:next-line
+//       let currentY = ev.pageY - ev.target.offsetTop;
+//       // @ts-ignore:next-line
+//       let width = currentX - this.startX;
+//       // @ts-ignore:next-line
+//       let height = currentY - this.startY;
+
+//       //@ts-ignore
+//       this.draw(
+//         // @ts-ignore:next-line
+//         this.startX,
+//         // @ts-ignore:next-line
+//         this.startY,
+//         width,
+//         height
+//       );
+//     }
+//   }
+
+//   public draw(x: number, y: number, width: number, height: number) {
+//     if (!this.ctx) return;
+
+//     this.ctx.rect(x, y, width, height);
+//     /** fill() - метод, отвечающий за заполнение фигуры цветом */
+//     this.ctx.fill();
+//     /** stroke() - метод, отвечающий за заполнение фигуры цветом */
+//     this.ctx.stroke();
+//   }
+// }
+
 import Tool from "./tool";
 
 export default class Rect extends Tool {
   public isMouseDown: boolean = false;
+  public startX: number = 0
+  public startY: number = 0
+ public saved: string =""
 
   constructor(canvas: HTMLCanvasElement) {
     super(canvas);
     this.listen();
   }
 
-  public listen() {
+  listen() {
     if (!this.canvas) return;
-
     this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
     this.canvas.onmousedown = this.mouseDownHandler.bind(this);
     this.canvas.onmouseup = this.mouseUpHandler.bind(this);
   }
 
-  public mouseUpHandler(ev: MouseEvent) {
+  mouseUpHandler(e: MouseEvent) {
     this.isMouseDown = false;
-  }
 
-  public mouseDownHandler(ev: MouseEvent) {
-    if (!this.ctx) return;
+  }
+  mouseDownHandler(e: MouseEvent) {
+
+    if( !this.ctx) return
+
     this.isMouseDown = true;
-    /** beginPath() - метод вызывается у контекста canvas, который говрит о начале отрисовки новой фигуры  */
     this.ctx.beginPath();
     // @ts-ignore:next-line
-    this.startX = ev.pageX - ev.target.offsetLeft;
+    this.startX = e.pageX - e.target.offsetLeft;
     // @ts-ignore:next-line
-    this.startY = ev.pageY - ev.target.offsetTop;
+    this.startY = e.pageY - e.target.offsetTop;
+    // @ts-ignore:next-line
+    this.saved = this.canvas.toDataURL();
   }
-
-  public mouseMoveHandler(ev: MouseEvent) {
+  mouseMoveHandler(e: MouseEvent) {
     if (this.isMouseDown) {
-      // @ts-ignore:next-line
-      let currentX = ev.pageX - ev.target.offsetLeft;
-      // @ts-ignore:next-line
-      let currentY = ev.pageY - ev.target.offsetTop;
-      // @ts-ignore:next-line
-      let width = currentX - this.startX;
-      // @ts-ignore:next-line
-      let height = currentY - this.startY;
-
-      //@ts-ignore
-      this.draw(
-        // @ts-ignore:next-line
-        this.startX,
-        // @ts-ignore:next-line
-        this.startY,
-        width,
-        height
-      );
+    // @ts-ignore:next-line
+      let currentX = e.pageX - e.target.offsetLeft;
+    // @ts-ignore:next-line
+      let currentY = e.pageY - e.target.offsetTop;
+    // @ts-ignore:next-line
+      this.width = currentX - this.startX;
+    // @ts-ignore:next-line
+      this.height = currentY - this.startY;
+    // @ts-ignore:next-line
+      this.draw(this.startX, this.startY, this.width, this.height);
     }
   }
 
-  public draw(x: number, y: number, width: number, height: number) {
-    if (!this.ctx) return;
-
-    this.ctx.rect(x, y, width, height);
-    /** fill() - метод, отвечающий за заполнение фигуры цветом */
-    this.ctx.fill();
-    /** stroke() - метод, отвечающий за заполнение фигуры цветом */
-    this.ctx.stroke();
+  draw(x: number, y: number, w: number, h: number) {
+    const img = new Image();
+    img.src = this.saved;
+    img.onload = () => {
+    // @ts-ignore:next-line
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    // @ts-ignore:next-line
+      this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
+    // @ts-ignore:next-line
+      this.ctx.beginPath();
+    // @ts-ignore:next-line
+      this.ctx.rect(x, y, w, h);
+    // @ts-ignore:next-line
+      this.ctx.fill();
+    // @ts-ignore:next-line
+      this.ctx.stroke();
+    };
   }
+
+
 }
