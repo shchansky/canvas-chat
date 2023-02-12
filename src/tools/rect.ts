1670da32e94af124cc1f91eeafe222c1452f926e
shchansky <gavrilenko71285@gmail.com>
2023-02-12 11:53:24 +0300

    commit

--- src/tools/rect.ts
+++ src/tools/rect.ts
@@ -1,71 +1,3 @@
-// import { throws } from "assert";
-// import Tool from "./tool";
-
-// export default class Rect extends Tool {
-//   public isMouseDown: boolean = false;
-
-//   constructor(canvas: HTMLCanvasElement) {
-//     super(canvas);
-//     this.listen();
-//   }
-
-//   public listen() {
-//     if (!this.canvas) return;
-
-//     this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
-//     this.canvas.onmousedown = this.mouseDownHandler.bind(this);
-//     this.canvas.onmouseup = this.mouseUpHandler.bind(this);
-//   }
-
-//   public mouseUpHandler(ev: MouseEvent) {
-//     this.isMouseDown = false;
-//   }
-
-//   public mouseDownHandler(ev: MouseEvent) {
-//     if (!this.ctx) return;
-//     this.isMouseDown = true;
-//     /** beginPath() - метод вызывается у контекста canvas, который говрит о начале отрисовки новой фигуры  */
-//     this.ctx.beginPath();
-//     // @ts-ignore:next-line
-//     this.startX = ev.pageX - ev.target.offsetLeft;
-//     // @ts-ignore:next-line
-//     this.startY = ev.pageY - ev.target.offsetTop;
-//   }
-
-//   public mouseMoveHandler(ev: MouseEvent) {
-//     if (this.isMouseDown) {
-//       // @ts-ignore:next-line
-//       let currentX = ev.pageX - ev.target.offsetLeft;
-//       // @ts-ignore:next-line
-//       let currentY = ev.pageY - ev.target.offsetTop;
-//       // @ts-ignore:next-line
-//       let width = currentX - this.startX;
-//       // @ts-ignore:next-line
-//       let height = currentY - this.startY;
-
-//       //@ts-ignore
-//       this.draw(
-//         // @ts-ignore:next-line
-//         this.startX,
-//         // @ts-ignore:next-line
-//         this.startY,
-//         width,
-//         height
-//       );
-//     }
-//   }
-
-//   public draw(x: number, y: number, width: number, height: number) {
-//     if (!this.ctx) return;
-
-//     this.ctx.rect(x, y, width, height);
-//     /** fill() - метод, отвечающий за заполнение фигуры цветом */
-//     this.ctx.fill();
-//     /** stroke() - метод, отвечающий за заполнение фигуры цветом */
-//     this.ctx.stroke();
-//   }
-// }
-
 import Tool from "./tool";
 
 export default class Rect extends Tool {
@@ -93,6 +25,7 @@ export default class Rect extends Tool {
     if (!this.ctx) return;
 
     this.isMouseDown = true;
+    /** beginPath() - метод вызывается у контекста canvas, который говрит о начале отрисовки новой фигуры  */
     this.ctx.beginPath();
     // @ts-ignore:next-line
     this.startX = e.pageX - e.target.offsetLeft;
@@ -128,8 +61,10 @@ export default class Rect extends Tool {
       this.ctx.beginPath();
       // @ts-ignore:next-line
       this.ctx.rect(x, y, w, h);
+      /** fill() - метод, отвечающий за заполнение фигуры цветом */
       // @ts-ignore:next-line
       this.ctx.fill();
+      /** stroke() - метод, отвечающий за заполнение фигуры цветом */
       // @ts-ignore:next-line
       this.ctx.stroke();
     };
