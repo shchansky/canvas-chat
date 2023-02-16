3a9676f9f1d9e17eed8ab37d79b7bc420473a459
shchansky <gavrilenko71285@gmail.com>
2023-02-16 20:09:22 +0300

    commit

--- src/components/settingbar/settingbar.tsx
+++ src/components/settingbar/settingbar.tsx
@@ -2,5 +2,19 @@ import React from "react";
 import * as Markup from "./settingbar.styles";
 
 export const Settingbar = () => {
-  return <Markup.Container>Settngbar</Markup.Container>;
+  return (
+    <Markup.Container>
+      <div>
+        <label htmlFor="line-width">Толщина линии</label>
+        <input
+          id="line-width"
+          type="number"
+          defaultValue={1}
+          min={1}
+          max={10}
+          style={{ margin: "0 10px" }}
+        />
+      </div>
+    </Markup.Container>
+  );
 };
